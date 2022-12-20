fa51301d0cfba5d39dc2763788ece88660a6151a
Allahnana <usmanallahnana@gmail.com>
2022-12-20 04:11:54 +0100

    Finished with the Business page

--- src/components/Billing.js
+++ src/components/Billing.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { apple, bill, google, } from '../assets'
+import styles from '../style'
+import { layout } from '../style'
 
-const Billing = () => {
-  return (
-    <div>Billing</div>
-  )
-}
+const Billing = () => (
+  
+    <section id='product' className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="billing" className='w-[100%] h-[100%] relative z-[5]'/>
+
+        <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient'/>
+        <div className='absolute z-[3] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient'/>
+      </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className='sm:block hidden'/> billing & invoicing.
+        </h2>
+
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          The key meaning of doings is when peace is absent where it is needed, therefore we call it aka Captain Vawulence. It is easily controlled when your temper is low.
+
+        </p>
+
+      </div>
+
+    </section>
+  
+)
 
 export default Billing
--- src/components/Business.js
+++ src/components/Business.js
@@ -3,14 +3,26 @@ import { features } from '../constants'
 import styles, { layout } from '../style'
 import Button from './Button'
 
-const FeatureCard = ({ icon, title, content, index}) => {
+const FeatureCard = ({ icon, title, content, index}) => (
 
-  <div>
-    <div>
-      <img src={icon} alt='icon'/>
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt='icon' className='w-[50%] h-[50%] object-contain '/>
+    </div>
+
+    <div className='flex-1 flex flex-col ml-3'>
+      <h4 className='font-poppins text-white text-[18px] font-semibold leading-[23px] mb-1 '>
+        {title}
+      </h4>
+
+      <p className='font-poppins text-dimWhite text-[16px] font-semibold leading-[24px] mb-1 '>
+        {content}
+      </p>
+
+
     </div>
   </div>
-}
+)
 
 
 const Business = () => {
